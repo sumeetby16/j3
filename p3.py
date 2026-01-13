adeaf95e1af3e42e4d091ee4d23039a29940720f
sumeetby16 <sumeetby16@gmail.com>
2026-01-13 19:34:14 +0530

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,10 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("Before swapping: a =", a, "b =", b)
+
+temp = a
+a = b
+b = temp
+
+print("After swapping: a =", a, "b =", b)
